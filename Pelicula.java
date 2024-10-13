42199fd57174a2f0b0a1f5178541ef42b5929dd1
Jordan <jordanmupe150@gmail.com>
2024-10-13 10:59:22 -0600

    Creacion de clase pelicula a medias

---
+++ Pelicula.java
@@ -0,0 +1,11 @@
+public class Pelicula {
+    private String nombre;
+    private int año;
+    private int duracion;
+    private String tipo;
+
+    public Pelicula(String nombre, int año, int duracion, String tipo){
+        this.nombre = nombre;
+    }
+
+}
