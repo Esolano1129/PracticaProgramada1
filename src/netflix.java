1e6ffdda8ea9cddada55e7ce66535207751d9198
Samifide <samiperez0311@gmail.com>
2024-10-16 09:13:27 -0600

    Terminado de menu
    samantha perez
    agregado de ultimos elementos del menu

--- src/netflix.java
+++ src/netflix.java
@@ -6,18 +6,18 @@ import java.util.List;
 import javax.imageio.plugins.tiff.TIFFImageReadParam;
 import java.util.*;
 
-public class netflix{
+public class netflix {
 
     public static void main(String[] args) {
 
-    List<Movie> movielist = new ArrayList<>();
+        List<Movie> movielist = new ArrayList<>();
 
-    movielist.add(new Drama("La lista de Schindler",1993, 3.5,"Drama", "Belico"));
-    movielist.add(new Anime("Howl's Moving Castle",2004, 2.0,"Anime", "Studio Ghibli"));
-    movielist.add(new Documental("Una verdad incómoda",2015, 3.0,"Documental", "Historico"));
-    movielist.add(new Anime("Mi vecino Totoro",1988, 1.5,"Anime", "Studio Ghibli"));
-    movielist.add(new Drama("Oppenheimer",2023, 3.0,"Drama", "Thrillers"));
-    movielist.add(new Documental("Escuadrón de la muerte",2019, 1.5,"Documental", "Accion"));
+        movielist.add(new Drama("La lista de Schindler", 1993, 3.5, "Drama", "Belico"));
+        movielist.add(new Anime("Howl's Moving Castle", 2004, 2.0, "Anime", "Studio Ghibli"));
+        movielist.add(new Documental("Una verdad incómoda", 2015, 3.0, "Documental", "Historico"));
+        movielist.add(new Anime("Mi vecino Totoro", 1988, 1.5, "Anime", "Studio Ghibli"));
+        movielist.add(new Drama("Oppenheimer", 2023, 3.0, "Drama", "Thrillers"));
+        movielist.add(new Documental("Escuadrón de la muerte", 2019, 1.5, "Documental", "Accion"));
 
 
         Scanner scanner = new Scanner(System.in);
@@ -30,58 +30,169 @@ public class netflix{
                 System.out.println("\nCatalogo de Netflix");
                 System.out.println("\nEliga una opcion: ");
                 System.out.println("1. Películas de Anime");
-                System.out.println("2. Películas de Drama");
-                System.out.println("3. Películas de Documental");
+                System.out.println("2. Películas de Documental");
+                System.out.println("3. Películas de Drama");
                 System.out.println("4. Todas las peliculas");
                 System.out.println("5. Salir");
 
                 int opc = scanner.nextInt();
-
+                boolean salir3 = false;
                 switch (opc) {
+
                     case 1:
-                        boolean salir3 = false;
-                        while(!salir3){
-                        for (Object o : movielist){
-                            boolean salir2 = false;
-                            if (o instanceof Anime) {
-                                while (!salir2){
 
-                                    try {
-                                        Anime anime1 = (Anime) o;
-                                        System.out.println("\n"+anime1.Name);
-                                        System.out.println("\nEliga una opcion: ");
-                                        System.out.println("1. Ver informacion completa");
-                                        System.out.println("2. Siguiente pelicula");
-                                        System.out.println("3. Salir");
-                                        int opc2 = scanner2.nextInt();
-                                        if (opc2 == 1){
-                                            System.out.println(anime1.toString());
-                                        } else if (opc2 == 2) {
-                                            salir2 = true;
-                                        } else if (opc2 ==3 ) {
-                                            salir2= true;
-                                            salir3 = true;
-                                            System.out.println("Saliendo al menu principal.");
-                                            break;
+                        while (!salir3) {
+                            for (Object o : movielist) {
+                                boolean salir2 = false;
+                                if (o instanceof Anime) {
+                                    while (!salir2) {
 
+                                        try {
+                                            Anime anime1 = (Anime) o;
+                                            System.out.println("\n" + anime1.Name);
+                                            System.out.println("\nEliga una opcion: ");
+                                            System.out.println("1. Ver informacion completa");
+                                            System.out.println("2. Siguiente pelicula");
+                                            System.out.println("3. Salir");
+                                            int opc2 = scanner2.nextInt();
+                                            if (opc2 == 1) {
+                                                System.out.println(anime1.toString());
+                                            } else if (opc2 == 2) {
+                                                salir2 = true;
+                                            } else if (opc2 == 3) {
+                                                salir2 = true;
+                                                salir3 = true;
+                                                System.out.println("Saliendo al menu principal.");
+                                                break;
+
+                                            }
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("Error: debe ingresar un numero.");
                                         }
-                                    }catch (InputMismatchException e) {
-                                        System.out.println("Error: debe ingresar un numero.");
                                     }
                                 }
-                            } if (salir3) {
-                                break;
+                                if (salir3) {
+                                    break;
+                                }
                             }
-                        }
                         }
                         break;
                     case 2:
+                        while (!salir3) {
+                            for (Object o : movielist) {
+                                boolean salir2 = false;
+                                if (o instanceof Documental) {
+                                    while (!salir2) {
+
+                                        try {
+                                            Documental documental1 = (Documental) o;
+                                            System.out.println("\n" + documental1.Name);
+                                            System.out.println("\nEliga una opcion: ");
+                                            System.out.println("1. Ver informacion completa");
+                                            System.out.println("2. Siguiente pelicula");
+                                            System.out.println("3. Salir");
+                                            int opc2 = scanner2.nextInt();
+                                            if (opc2 == 1) {
+                                                System.out.println(documental1.toString());
+                                            } else if (opc2 == 2) {
+                                                salir2 = true;
+                                            } else if (opc2 == 3) {
+                                                salir2 = true;
+                                                salir3 = true;
+                                                System.out.println("Saliendo al menu principal.");
+                                                break;
+
+                                            }
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("Error: debe ingresar un numero.");
+                                        }
+                                    }
+                                }
+                                if (salir3) {
+                                    break;
+                                }
+                            }
+                        }
 
                         break;
                     case 3:
+                        while (!salir3) {
+                            for (Object o : movielist) {
+                                boolean salir2 = false;
+                                if (o instanceof Drama) {
+                                    while (!salir2) {
 
+                                        try {
+                                            Drama drama1 = (Drama) o;
+                                            System.out.println("\n" + drama1.Name);
+                                            System.out.println("\nEliga una opcion: ");
+                                            System.out.println("1. Ver informacion completa");
+                                            System.out.println("2. Siguiente pelicula");
+                                            System.out.println("3. Salir");
+                                            int opc2 = scanner2.nextInt();
+                                            if (opc2 == 1) {
+                                                System.out.println(drama1.toString());
+                                            } else if (opc2 == 2) {
+                                                salir2 = true;
+                                            } else if (opc2 == 3) {
+                                                salir2 = true;
+                                                salir3 = true;
+                                                System.out.println("Saliendo al menu principal.");
+                                                break;
+
+                                            }
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("Error: debe ingresar un numero.");
+                                        }
+                                    }
+                                }
+                                if (salir3) {
+                                    break;
+                                }
+                            }
+                        }
                         break;
+
+
                     case 4:
+
+
+                        while (!salir3) {
+                            for (Object o : movielist) {
+                                boolean salir2 = false;
+                                if (o instanceof Movie) {
+                                    while (!salir2) {
+
+                                        try {
+                                            Movie Movie1 = (Movie) o;
+                                            System.out.println("\n" + Movie1.Name);
+                                            System.out.println("\nEliga una opcion: ");
+                                            System.out.println("1. Ver informacion completa");
+                                            System.out.println("2. Siguiente pelicula");
+                                            System.out.println("3. Salir");
+                                            int opc2 = scanner2.nextInt();
+                                            if (opc2 == 1) {
+                                                System.out.println(Movie1.toString());
+                                            } else if (opc2 == 2) {
+                                                salir2 = true;
+                                            } else if (opc2 == 3) {
+                                                salir2 = true;
+                                                salir3 = true;
+                                                System.out.println("Saliendo al menu principal.");
+                                                break;
+
+                                            }
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("Error: debe ingresar un numero.");
+                                        }
+                                    }
+                                }
+                                if (salir3) {
+                                    break;
+                                }
+                            }
+                        }
+
 
                         break;
                     case 5:
@@ -98,6 +209,5 @@ public class netflix{
     }
 
 
-
-    }
+}
 
